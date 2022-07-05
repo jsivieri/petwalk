fb823718de031e0487453f6c53616c6e7c0bb8f7
Flavius Atila <flaviusatila@gmail.com>
2022-07-05 19:43:24 -0300

    Primeiro

---
+++ src/main/java/com/br/petwalk/PetwalkApplication.java
@@ -0,0 +1,13 @@
+package com.br.petwalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetwalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetwalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/petwalk/PetwalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.petwalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetwalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
